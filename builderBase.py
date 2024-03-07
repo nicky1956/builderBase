8c78d96071bd9b1f48bcdaa1fd35ab66cd20571b
NickyP <nicky1956@gmail.com>
2024-03-07 15:15:50 -0500

    added while loop and auto collect elixir

--- builderBase.py
+++ builderBase.py
@@ -4,6 +4,7 @@ import pyautogui
 import pygetwindow as gw
 import time
 import pydirectinput
+import signal
 
 def locate_play_button():
     target_monitor_title = 'Google Play Games beta'
@@ -57,7 +58,7 @@ def click_play_button(coordinates):
     else:
         print("Green play button not found.")
         
-def wait_for_window(title, timeout=60):
+def wait_for_window(title, timeout=10):
     start_time = time.time()
 
     while time.time() - start_time < timeout:
@@ -71,8 +72,7 @@ def wait_for_window(title, timeout=60):
     return False
         
 def getClashWindow():
-    target_monitor_title = 'Clash of Clans'
-    target_monitor = gw.getWindowsWithTitle(target_monitor_title)
+    target_monitor = gw.getWindowsWithTitle('Clash of Clans')
 
     if target_monitor:
         target_monitor = target_monitor[0]
@@ -85,6 +85,23 @@ def getClashWindow():
         threshold_image = cv2.cvtColor(screenshot_np, cv2.COLOR_RGB2BGR)
 
     return target_monitor, threshold_image
+
+def move_mouse_to_middle_of_window():
+    window_title = 'Clash of Clans'
+    target_window = gw.getWindowsWithTitle(window_title)
+
+    if target_window:
+        target_window = target_window[0]
+
+        # Calculate the coordinates of the middle of the window
+        middle_x = target_window.left + target_window.width // 2
+        middle_y = target_window.top + target_window.height // 2
+
+        # Move the mouse to the middle of the window
+        pyautogui.moveTo(middle_x, middle_y)
+        print("Mouse moved to the middle of the window.")
+    else:
+        print(f"Window not found: {window_title}")
 
 def close_clash_of_clans():
     target_window_title = 'Clash of Clans'
@@ -146,6 +163,8 @@ def click_image_location(template_paths, bottom_right=False):
         # Rescale the template based on the screenshot resolution
         width_scale = threshold_image.shape[1] / 2422
         height_scale = threshold_image.shape[0] / 1393
+        
+        print("Width_Scale:",width_scale,"\nHieght_Scale:",height_scale)
         template = cv2.resize(template, None, fx=width_scale, fy=height_scale)
 
         # Use template matching to find the location of the template in the threshold image
@@ -177,34 +196,62 @@ def click_image_location(template_paths, bottom_right=False):
 
     print("No matching templates found.")
 
-
-#find play button and then click it
-play_button_location = locate_play_button()
-click_play_button(play_button_location)
-
-#wait for COC to launch
-wait_for_window('Clash of Clans')
-
-#wait for COC to load once it is launched
-time.sleep(7)
-
-#use attack template to find and click
-click_image_location([r"assets\attack.png",r"assets\attack2.png"])
-
-#use  find now template to find and click, wait for attack click to go through
-time.sleep(1)
-click_image_location(r"assets\findnow.png")
-
-#scroll all the way out and then drag down so we see top half of the base
-drag_to_bottom()
-
-#deploy troops
-click_coordinates = click_image_location(
-    [r"assets\secondDirty.png",r"assets\secondClean.png"],bottom_right=True)
-key_presses = ['q','1','2','3','4','5','6','7','8','9','0']
-for k in key_presses:
-    pydirectinput.press(k)
-    pyautogui.click(click_coordinates[0], click_coordinates[1])
-
-#close clash of clans
-close_clash_of_clans()
+def main():
+    #find play button and then click it
+    play_button_location = locate_play_button()
+    if play_button_location == None:
+        return
+    click_play_button(play_button_location)
+    
+    #wait for COC to launch
+    wait_for_window('Clash of Clans')
+    
+    #wait for COC to load once it is launched
+    time.sleep(7)
+    
+    #scroll all the way out and then drag down so we see top half of the base
+    move_mouse_to_middle_of_window()
+    pyautogui.scroll(-10000)
+    pyautogui.scroll(-10000)
+    pyautogui.scroll(-10000)
+    pyautogui.scroll(-10000)
+    pyautogui.scroll(-10000)
+    pyautogui.scroll(-10000)
+    
+    #click on the elixir cart
+    click_image_location([r"assets\elixirElixir.png", r"assets\elixirFight.png",
+                          r"assets\elixirBase.png"])
+    
+    #click on the collect button
+    time.sleep(.5)
+    click_image_location([r"assets\collect.png"])
+    pydirectinput.press('esc')
+    time.sleep(1.5)
+    
+    #use attack template to find and click
+    click_image_location([r"assets\attack.png",r"assets\attack2.png"])
+    
+    #use  find now template to find and click, wait for attack click to go through
+    time.sleep(1)
+    click_image_location(r"assets\findnow.png")
+    
+    #scroll all the way out and then drag down so we see top half of the base
+    drag_to_bottom()
+    
+    #deploy troops
+    click_coordinates = click_image_location(
+        [r"assets\secondDirty.png",r"assets\secondClean.png"],bottom_right=True)
+    if click_coordinates == None:
+        close_clash_of_clans()
+        return
+    key_presses = ['q','1','2','3','4','5','6','7','8','9','0']
+    for k in key_presses:
+        pydirectinput.press(k)
+        pyautogui.click(click_coordinates[0], click_coordinates[1])
+    
+    #close clash of clans
+    close_clash_of_clans()
+    
+if __name__ == "__main__":
+    while True:
+        main()
